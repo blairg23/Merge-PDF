0f545e781aba1e15e2879507bfd208f53fff4767
BlairG <blairg23@gmail.com>
2021-05-01 23:44:36 -0600

    Added support for python3, added requirements.txt

--- merge_all_pdfs.py
+++ merge_all_pdfs.py
@@ -7,7 +7,7 @@ pdf_files = [f for f in os.listdir(files_dir) if f.endswith("pdf")]
 merger = PdfFileMerger()
 
 for filename in pdf_files:
-	print "Adding {filename} to the file.".format(filename=filename)
+	print(f"Adding {filename} to the file.")
 	merger.append(PdfFileReader(os.path.join(files_dir, filename), "rb"))
 
 merger.write(os.path.join(files_dir, merged_file+".pdf"))
